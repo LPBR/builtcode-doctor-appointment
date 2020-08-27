fd8f8c80699904045fd269e88ccca47dfede5a56
rachu <lucas.p.sec@gmail.com>
2020-08-27 01:48:50 -0300

    Move locale definition to application.rb so it's used on all environments

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,15 @@ module BuiltcodeDoctorAppointment
 
     config.time_zone = 'Brasilia'
 
+    # Where the I18n library should search for translation files
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :"pt-BR"]
+
+    # Set default locale to something other than :en
+    I18n.default_locale = :"pt-BR"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
