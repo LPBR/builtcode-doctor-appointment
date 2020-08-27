00387b01b33bf9fff712df32425e36e94d216599
rachu <lucas.p.sec@gmail.com>
2020-08-27 00:31:11 -0300

    Refactor display list of registered doctors specs

--- spec/features/display_list_of_registered_doctors_spec.rb
+++ spec/features/display_list_of_registered_doctors_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "DisplayListOfRegisteredDoctors", type: :feature do
   scenario "Without registered doctors" do
-    visit "/doctors"
+    navigate_to_doctors_index
+
     expect(page).to have_css ".empty-list", text: "Nenhum médico cadastrado"
   end
 
   scenario "With registered doctors" do
     doctor = create(:doctor)
 
-    visit "/doctors"
+    navigate_to_doctors_index
 
     expect(page).to have_css "td", text: doctor.name
     expect(page).to have_css "td", text: "#{doctor.crm}/#{doctor.crm_uf}"
---
+++ spec/support/features/doctors_helper.rb
@@ -0,0 +1,6 @@
+module Features
+  def navigate_to_doctors_index
+    visit "/"
+    click_on "Médicos"
+  end
+end
