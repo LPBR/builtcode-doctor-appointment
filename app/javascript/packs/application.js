1b5e3d40699865ea5b34444b546832b5f132dde4
rachu <lucas.p.sec@gmail.com>
2020-08-30 04:26:59 -0300

    Install jquery mask plugin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,16 @@
 import "bootstrap"
-
+import "jquery-mask-plugin"
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
 require("@fortawesome/fontawesome-free")
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
   $('.toast').toast('show')
+
+  $('.date-time').mask('00/00/0000 00:00');
+  $('.date').mask('00/00/0000')
+  $('.number-only').mask('0#')
+  $('.cpf').mask('000.000.000-00', {reverse: true})
 })
